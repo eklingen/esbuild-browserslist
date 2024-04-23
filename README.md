e7cca0ffd06a3700af7f3cf7e0009e1d701060ed
Elco Klingen <disillusion84@gmail.com>
2024-04-23 22:20:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# ESBuild Browserslist
+
+Use Browserslist definition as ESBuild targets.
+
+## Usage
+
+Add the function to the `target` field of your `esbuild.config.js`:
+
+```javascript
+import getBrowserslistTargets from '@eklingen/esbuild-browserslist'
+target: getBrowserslistTargets(),
+```
+
+This will make ESBuild use your `.browserslistrc` as the target definition, where possible.
+
+## Notes & caveats
+
+### 1. Unsupported target names
+
+The following browserslist targets are NOT supported:
+`android`, `and_qq`, `and_uc`, `baidu`, `bb`, `kaios`, `op_mob` and `op_mini`.
+(for android, choose an equivalent target like "chrome", and for "op_mob" choose an equivalent target like "opera")
+
+### 3. Unsupported target versions
+
+The following target versions are NOT supported:
+`all` and `TP` (as in `op_mob all` and `safari TP`).
+
+### 3. Supported targets
+
+The following browserslist targets ARE supported:
+"chrome", "edge", "firefox", "ie", "opera", "safari" and "samsung".
+
+### 4. Targets mapped to equivalents
+
+The following browserlist targets are mapped to equivalents with the same version number:
+`and_chr` to `chrome`, "and_ff" to "firefox", "ie_mob" to "ie" and "ios_saf" to "safari".
+
+### 5. Version ranges behavior
+
+For minor versions or ranges (like `ios_saf 12.1-13.3`), the oldest version specified is used (`ios_saf 12`).
+
+## Dependencies
+
+This package requires ["browserslist"](https://www.npmjs.com/package/browserslist).
+
+---
+
+Copyright (c) 2024 Elco Klingen. MIT License.
---
+++ index.js
@@ -0,0 +1,44 @@
+import browserslist from 'browserslist'
+
+/* About Browserlist targets vs ESBuild targets:
+ * - The following browserslist targets are NOT supported:
+ *   "android", "and_qq", "and_uc", "baidu", "bb", "kaios", "op_mob" and "op_mini".
+ *   (for android, choose an equivalent target like "chrome", and for "op_mob" choose an equivalent target like "opera")
+ * - The following browserslist target versions are NOT supported:
+ *   "all" and "tp" (as in "op_mob all" or "safari TP").
+ * - The following browserslist targets ARE supported:
+ *   "chrome", "edge", "firefox", "ie", "opera", "safari" and "samsung".
+ * - The following browserlist targets are mapped to equivalents with the same version number:
+ *   "and_chr" to "chrome", "and_ff" to "firefox", "ie_mob" to "ie" and "ios_saf" to "safari".
+ * - For minor versions or ranges (like ios_saf 12.1-13.3), the oldest version specified is used.
+ */
+
+const TARGETS = ['chrome', 'edge', 'firefox', 'ie', 'opera', 'safari', 'samsung']
+const ALIASES = { and_chr: 'chrome', and_ff: 'firefox', ie_mob: 'ie', ios_saf: 'safari' }
+const REGEX = /([0-9]+)(.*)/
+
+// Convert the browserslist config to an ESBuild compatible array of targets
+export default function getBrowserslistTargets() {
+  let list = browserslist(browserslist.loadConfig({ path: process.cwd() }))
+
+  // Split names and numbers so we can operate on names only
+  list = list.map(item => item.split(' '))
+  // Filter out bogus versions such as "TP" or "all"
+  list = list.filter(([name, number]) => REGEX.test(number))
+  // Replace any items with their aliases
+  list = list.map(([name, number]) => [ALIASES[name] || name, number])
+  // Filter out unsupported targets
+  list = list.filter(([name, number]) => TARGETS.includes(name))
+  // Replace version ranges with the oldest version in the range
+  list = list.map(([name, number]) => [name, number.replace(REGEX, '$1') || number])
+  // Filter out all values with the same name but a larger number value
+  list = list.filter(([name, number], index, self) => self.findIndex(([otherName, otherNumber]) => otherName === name && otherNumber < number) === -1)
+  // Join names and numbers back together
+  list = list.map(item => item.join(''))
+  // Sort the list alphabetically
+  list = list.sort()
+  // Remove duplicates and sort the list
+  list = [...new Set(list)].sort()
+
+  return list
+}
